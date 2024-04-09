db483b582a5cc26b5e6b89ea42dd93c607914d74
Sylvain Leroy <sleroy@byoskill.com>
2024-04-09 12:36:33 +0200

    Initialisation project

---
+++ monster-adoption-factory/frontend-monster-adoption-store/src/main/java/com/byoskill/controllers/HomePage.java
@@ -0,0 +1,25 @@
+package com.byoskill.controllers;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import io.quarkus.qute.TemplateInstance;
+import io.quarkus.qute.Template;
+
+@Path("")
+public class HomePage {
+
+    @Inject
+    Template index; 
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance get() {
+        return index.instance();  
+    }
+
+}
