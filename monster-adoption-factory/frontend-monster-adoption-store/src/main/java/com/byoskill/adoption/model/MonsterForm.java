721cb11b3c3c127947d9a27ae321bfa604afa749
Sylvain Leroy <sleroy@byoskill.com>
2024-04-17 10:06:21 +0200

    Monster form

---
+++ monster-adoption-factory/frontend-monster-adoption-store/src/main/java/com/byoskill/adoption/model/MonsterForm.java
@@ -0,0 +1,50 @@
+package com.byoskill.adoption.model;
+
+public class MonsterForm {
+    private String name;
+    private String type;
+    private String description;
+    private Integer price;    
+    private String location;
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+}
