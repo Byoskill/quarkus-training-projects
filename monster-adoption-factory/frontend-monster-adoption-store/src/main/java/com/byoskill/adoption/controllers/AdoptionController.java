d36651af1d705b460427c259f3ff0528ae04cd56
Sylvain Leroy <sleroy@byoskill.com>
2024-04-17 09:58:20 +0200

    Controller resource

--- monster-adoption-factory/frontend-monster-adoption-store/src/main/java/com/byoskill/adoption/controllers/AdoptionController.java
+++ monster-adoption-factory/frontend-monster-adoption-store/src/main/java/com/byoskill/adoption/controllers/AdoptionController.java
@@ -2,6 +2,7 @@ package com.byoskill.adoption.controllers;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
+import com.byoskill.adoption.model.MonsterForm;
 import com.byoskill.communication.client.CommunicationMessageService;
 import com.byoskill.communication.model.WelcomeMessage;
 
@@ -9,10 +10,14 @@ import io.quarkus.qute.Template;
 import io.quarkus.qute.TemplateInstance;
 import io.smallrye.common.annotation.Blocking;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.FormParam;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/adoptions")
 public class AdoptionController {
@@ -21,13 +26,35 @@ public class AdoptionController {
     //CommunicationMessageService communicationMessageService;
     
     @Inject
-    Template index; 
+    Template adoptionForm; 
 
     @Blocking
     @GET
     @Produces(MediaType.TEXT_HTML)
     public TemplateInstance get() {
         //WelcomeMessage welcomeMessage = communicationMessageService.getWelcomeMessage();
-        return index.instance();
+        return adoptionForm.instance();
+    }
+ @POST
+    @Path("/add")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response addMonster(
+            @FormParam("monsterUUID") String monsterUUID,
+            @FormParam("name") String name,
+            @FormParam("description") String description,
+            @FormParam("price") Integer price,
+            @FormParam("age") Integer age,
+            @FormParam("location") String location) {
+
+        MonsterForm monster = new MonsterForm();
+        monster.setName(name);
+        monster.setDescription(description);
+        monster.setPrice(price);
+        monster.setAge(age);
+        monster.setLocation(location);
+
+        monsterService.addMonster(monster);
+
+        return Response.ok().build();
     }
 }
