640f9e581ea29d4fa936638c6e34dbee1aee0d2c
Sylvain Leroy <contact@sylvainleroy.com>
2024-06-01 18:39:09 +0200

    Corrigé

---
+++ monster-adoption-factory/frontend-monster-adoption-store/src/main/java/controllers/HomePage.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import io.quarkus.qute.TemplateInstance;
+import io.quarkus.qute.Template;
+
+@Path("")
+public class HomePage {
+
+    @Inject
+    Template index;
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance get() {
+        return index.data("motd", "Bienvenue sur notre boutique");  
+    }
+
+}
