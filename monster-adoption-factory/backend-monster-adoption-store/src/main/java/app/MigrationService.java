c83b546e680532e55d951418776aebefb531b6b4
Sylvain Leroy <sleroy0@gmail.com>
2024-06-05 17:04:05 +0200

    Migration service

---
+++ monster-adoption-factory/backend-monster-adoption-store/src/main/java/app/MigrationService.java
@@ -0,0 +1,19 @@
+package app;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.flywaydb.core.Flyway;
+
+@ApplicationScoped
+public class MigrationService {
+    // Vous pouvez injecter l'objet si vous voulez l'utiliser manuellement
+    @Inject
+    Flyway flyway;
+
+    @PostConstruct
+    public void checkMigration() {
+        // Cela affichera 1.0.0
+        System.out.println(flyway.info().current().getVersion().toString());
+    }
+}
